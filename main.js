8c8c61437633c49f7e69f818c8862ee126b40f3c
PHO3NIX5 <92304496+PHO3NIX5@users.noreply.github.com>
2022-12-07 17:53:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 
+
+
+var paddle2 =10,paddle1=10;
+
 var paddle1X = 10,paddle1Height = 110;
 var paddle2Y = 685,paddle2Height = 70;
 
@@ -162,11 +166,11 @@ function move(){
   if (ball.x-2.5*ball.r/2< 0){
   if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
     ball.dx = -ball.dx+0.5; 
-    
+    ball_touch_paddel.play();
   }
   else{
     pcscore++;
-    
+    missed.play();
     reset();
     navigator.vibrate(100);
   }
@@ -179,7 +183,7 @@ if(pcscore ==4){
     stroke("white");
     textSize(25);
     text("Game Over!",width/2,height/2);
-    text("Reload the page!",width/2,height/2+30)
+    text("Press Restart button to play again!",width/2,height/2+30)
     noLoop();
     pcscore = 0;
  }
@@ -210,4 +214,14 @@ function paddleInCanvas(){
   }
  
   
+}
+
+function preload(){
+  ball_touch_paddel = loadSound("ball_touch_paddel.wav");
+  missed = loadSound("missed.wav");
+}
+function restart(){
+  pcscore = 0;
+  playerscore = 0;
+  loop();
 }
